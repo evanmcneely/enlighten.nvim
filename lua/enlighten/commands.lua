2d536a8122ae69c6028da7be6d04d73b0d6462ac
Evan McNeely <evan.mcneely@leadpages.com>
2024-07-10 07:36:51 -0700

    factor logic for getting selection or cursor range into seperate functions

--- lua/enlighten/commands.lua
+++ lua/enlighten/commands.lua
@@ -10,51 +10,89 @@ local system_prompt = [[
       explanations or summarys. Do not include code block markdown starting with ```.
 ]]
 
+---@class Position
+---@field col_start number
+---@field row_start number
+---@field col_end number
+---@field row_end number
+
+---@param buffer number
+---@return Position
+local function get_selection_range(buffer)
+	-- Get column and row from for the start of the selection range.
+	local start_pos = vim.api.nvim_buf_get_mark(buffer, "<")
+	local row_start = start_pos[1] - 1 -- adjust for lua's 1 based indexing
+	local col_start = start_pos[2]
+
+	-- Get the column and row for the end of the selection range.
+	local end_pos = vim.api.nvim_buf_get_mark(buffer, ">")
+	local row_end = end_pos[1] - 1 -- adjust for lua's 1 based indexing
+	-- Look at the actual content of the line at the end row.
+	-- If the line is empty, set col_end to zero. Otherwise, compute
+	-- col_end via the byte index in the string, considering any multi-byte
+	-- characters if present.
+	local line = vim.fn.getline(end_pos[1])
+	local col_end
+	if line == "" then
+		col_end = 0
+	else
+		col_end = vim.fn.byteidx(line, vim.fn.charcol("'>"))
+	end
+
+	-- Adjustments to ensure column start and end do not exceed the actual length of the line.
+	local start_line_length = vim.api.nvim_buf_get_lines(buffer, row_start, row_start + 1, true)[1]:len()
+	col_start = math.min(col_start, start_line_length)
+	local end_line_length = vim.api.nvim_buf_get_lines(buffer, row_end, row_end + 1, true)[1]:len()
+	col_end = math.min(col_end, end_line_length)
+
+	return { col_start = col_start, col_end = col_end, row_start = row_start, row_end = row_end }
+end
+
+---@return Position
+local function get_cursor_position(buffer)
+	-- Get column and row from for the cursor position.
+	local start_pos = vim.api.nvim_win_get_cursor(0)
+	local row_start = start_pos[1] - 1 -- adjust for lua's 1 based indexing
+	-- Look at the actual content of the line at start position.
+	-- If the line is empty, set col_end to zero. Otherwise, compute
+	-- col_end via the byte index in the string, considering any multi-byte
+	-- characters if present.
+	local line = vim.fn.getline(start_pos[1])
+	local col_start
+	if line == "" then
+		col_start = 0
+	else
+		col_start = vim.fn.byteidx(line, vim.fn.charcol("."))
+	end
+
+	-- Start and end values are the same.
+	local col_end = col_start
+	local row_end = row_start
+
+	-- Adjustments to ensure column start and end do not exceed the actual length of the line.
+	local start_line_length = vim.api.nvim_buf_get_lines(buffer, row_start, row_start + 1, true)[1]:len()
+	col_start = math.min(col_start, start_line_length)
+	local end_line_length = vim.api.nvim_buf_get_lines(buffer, row_end, row_end + 1, true)[1]:len()
+	col_end = math.min(col_end, end_line_length)
+
+	return { col_start = col_start, col_end = col_end, row_start = row_start, row_end = row_end }
+end
+
 ---@param args { args: string, range: integer }
 function M.ai(args)
 	local prompt = args.args
-	local visual_mode = args.range > 0
-
+	local selection = args.range > 0
 	local buffer = vim.api.nvim_get_current_buf()
 
-	local start_row, start_col
-	local end_row, end_col
+	local range
 
-	if visual_mode then
-		-- Use the visual selection
-		local start_pos = vim.api.nvim_buf_get_mark(buffer, "<")
-		start_row = start_pos[1] - 1
-		start_col = start_pos[2]
-
-		local end_pos = vim.api.nvim_buf_get_mark(buffer, ">")
-		end_row = end_pos[1] - 1
-		local line = vim.fn.getline(end_pos[1])
-		if line == "" then
-			end_col = 0
-		else
-			end_col = vim.fn.byteidx(line, vim.fn.charcol("'>"))
-		end
+	if selection then
+		range = get_selection_range(buffer)
 	else
-		-- Use the cursor position
-		local start_pos = vim.api.nvim_win_get_cursor(0)
-		start_row = start_pos[1] - 1
-		local line = vim.fn.getline(start_pos[1])
-		if line == "" then
-			start_col = 0
-		else
-			start_col = vim.fn.byteidx(line, vim.fn.charcol("."))
-		end
-		end_row = start_row
-		end_col = start_col
+		range = get_cursor_position(buffer)
 	end
 
-	local start_line_length = vim.api.nvim_buf_get_lines(buffer, start_row, start_row + 1, true)[1]:len()
-	start_col = math.min(start_col, start_line_length)
-
-	local end_line_length = vim.api.nvim_buf_get_lines(buffer, end_row, end_row + 1, true)[1]:len()
-	end_col = math.min(end_col, end_line_length)
-
-	local indicator_obj = indicator.create(buffer, start_row, start_col, end_row, end_col)
+	local indicator_obj = indicator.create(buffer, range.row_start, range.col_start, range.row_end, range.col_end)
 	local accumulated_text = ""
 
 	local function on_data(data)
@@ -69,16 +107,18 @@ function M.ai(args)
 
 	local function on_complete(err)
 		if err then
-			vim.api.nvim_err_writeln("enlightened.nvim :" .. err)
+			vim.api.nvim_err_writeln("enlighten.nvim :" .. err)
 		elseif #accumulated_text > 0 then
 			indicator.set_buffer_text(indicator_obj, accumulated_text)
 		end
 		indicator.finish(indicator_obj)
 	end
 
-	if visual_mode then
-		local selected_text =
-			table.concat(vim.api.nvim_buf_get_text(buffer, start_row, start_col, end_row, end_col, {}), "\n")
+	if selection then
+		local selected_text = table.concat(
+			vim.api.nvim_buf_get_text(buffer, range.row_start, range.col_start, range.row_end, range.col_end, {}),
+			"\n"
+		)
 		if prompt == "" then
 			-- Replace the selected text, also using it as a prompt
 			openai.completions({
@@ -111,10 +151,10 @@ function M.ai(args)
 			local prefix = table.concat(
 				vim.api.nvim_buf_get_text(
 					buffer,
-					math.max(0, start_row - config.context_before),
+					math.max(0, range.row_start - config.context_before),
 					0,
-					start_row,
-					start_col,
+					range.row_start,
+					range.col_start,
 					{}
 				),
 				"\n"
@@ -124,9 +164,9 @@ function M.ai(args)
 			local suffix = table.concat(
 				vim.api.nvim_buf_get_text(
 					buffer,
-					end_row,
-					end_col,
-					math.min(end_row + config.context_after, line_count - 1),
+					range.row_end,
+					range.col_end,
+					math.min(range.row_end + config.context_after, line_count - 1),
 					99999999,
 					{}
 				),
