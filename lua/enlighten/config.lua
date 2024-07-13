a4f9b986c7d4497942aab5a9a68a3ab87d3c4a21
Evan McNeely <evan.mcneely@leadpages.com>
2024-07-12 22:19:21 -0700

    increase default amout of code context

--- lua/enlighten/config.lua
+++ lua/enlighten/config.lua
@@ -13,8 +13,8 @@ end
 
 M.completions_model = get_var("ai_completions_model", "gpt-4o")
 M.temperature = get_var("ai_temperature", 0)
-M.context_before = get_var("ai_context_before", 20)
-M.context_after = get_var("ai_context_after", 20)
+M.context_before = get_var("ai_context_before", 100)
+M.context_after = get_var("ai_context_after", 100)
 M.timeout = get_var("ai_timeout", 60)
 M.tokens = get_var("ai_tokens", 4096)
 
