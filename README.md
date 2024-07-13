2c3b186ec141bebe5b0f4c8fc4a97ad005151f11
Evan McNeely <evan.mcneely@leadpages.com>
2024-07-13 15:59:58 -0700

    move prefix/suffix into cursor prompt control flow

--- README.md
+++ README.md
@@ -4,8 +4,9 @@ A minimalist Neovim plugin for generating and editing text using AI.
 
 ### TODO
 
-- config in setup function
-- gpt-4o default
-- anthropic (model interface)
-- hightlight groups for diff add/remove
-- popup input instead of command
+- [x] gpt-4o default
+- [ ] respect beginning line column start position when inserting text
+- [ ] hightlight groups for diff add/remove
+- [ ] popup input instead of command
+- [ ] config in setup function
+- [ ] anthropic (model interface)
--- lua/enlighten/commands.lua
+++ lua/enlighten/commands.lua
@@ -22,44 +22,44 @@ function M.ai(args)
 	local writer = Writer:new(buffer, range)
 	local file = utils.get_file_extension(buffer)
 
-	-- Insert some text generated using surrounding context
-	local prefix = table.concat(
-		vim.api.nvim_buf_get_text(
-			buffer,
-			math.max(0, range.row_start - config.context_before),
-			0,
-			range.row_start,
-			range.col_start,
-			{}
-		),
-		"\n"
-	)
-
-	local line_count = vim.api.nvim_buf_line_count(buffer)
-	local suffix = table.concat(
-		vim.api.nvim_buf_get_text(
-			buffer,
-			range.row_end,
-			range.col_end,
-			math.min(range.row_end + config.context_after, line_count - 1),
-			99999999,
-			{}
-		),
-		"\n"
-	)
-
 	if selection then
 		local selected_text = table.concat(
 			vim.api.nvim_buf_get_text(buffer, range.row_start, range.col_start, range.row_end, range.col_end, {}),
 			"\n"
 		)
+
 		if prompt == "" then
 			prompt = "Rewrite this code for simplicity and clarity:\n\n"
 		else
 			prompt = "Rewrite this code following these instructions: " .. prompt .. "\n\n"
 		end
+
 		ai.complete("File extension of the buffer is " .. file .. ". " .. prompt .. selected_text, writer)
 	else
+		local prefix = table.concat(
+			vim.api.nvim_buf_get_text(
+				buffer,
+				math.max(0, range.row_start - config.context_before),
+				0,
+				range.row_start,
+				range.col_start,
+				{}
+			),
+			"\n"
+		)
+		local line_count = vim.api.nvim_buf_line_count(buffer)
+		local suffix = table.concat(
+			vim.api.nvim_buf_get_text(
+				buffer,
+				range.row_end,
+				range.col_end,
+				math.min(range.row_end + config.context_after, line_count - 1),
+				99999999,
+				{}
+			),
+			"\n"
+		)
+
 		if prompt == "" then
 			prompt =
 				"Write the code that completes the snippet below at the line -- insert here --. Don't repeat the code before or after this line.\n\n"
@@ -68,6 +68,7 @@ function M.ai(args)
 				.. prompt
 				.. ". Your code snippet will be inserted into the buffer at the line -- insert here --. Don't repeat the code before or after this line.\n\n"
 		end
+
 		ai.complete(
 			"File extension of the buffer is " .. file .. ". " .. prompt .. prefix .. "\n-- insert here --\n" .. suffix,
 			writer
