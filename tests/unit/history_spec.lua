15540088daf80f8c8377b0ec9a5b15c210f77b21
Evan McNeely <evan.mcneely@leadpages.com>
2024-08-23 21:46:03 -0700

    start to fix history tests

--- tests/unit/history_spec.lua
+++ tests/unit/history_spec.lua
@@ -1,86 +1,61 @@
 local History = require("enlighten.history")
-local buffer = require("enlighten.buffer")
-local tu = require("tests.testutils")
 
 local equals = assert.are.same
 
 describe("history", function()
   local content = "abc"
-  local buf
-
-  before_each(function()
-    buf = tu.prepare_buffer(content)
-  end)
 
   it("should not scroll when there is no history", function()
-    local h = History:new(buf, {})
+    local h = History:new({})
 
-    h:scroll_back()
-    equals(content, buffer.get_content(buf))
+    local got = h:scroll_back()
+    equals(nil, got)
 
-    h:scroll_forward()
-    equals(content, buffer.get_content(buf))
+    got = h:scroll_forward()
+    equals(nil, got)
   end)
 
   it("should scroll backwards and forwards when there is history", function()
-    local h = History:new(buf, { { "1" }, { "2" }, { "3" } })
+    local h = History:new({ { "1" }, { "2" }, { "3" } })
 
-    h:scroll_back()
-    equals("1", buffer.get_content(buf))
+    equals({ "1" }, h:scroll_back())
 
-    h:scroll_back()
-    equals("2", buffer.get_content(buf))
+    equals({ "2" }, h:scroll_back())
 
-    h:scroll_back()
-    equals("3", buffer.get_content(buf))
+    equals({ "3" }, h:scroll_back())
 
-    h:scroll_forward()
-    equals("2", buffer.get_content(buf))
+    equals({ "2" }, h:scroll_forward())
 
-    h:scroll_forward()
-    equals("1", buffer.get_content(buf))
-
-    h:scroll_forward()
-    equals(content, buffer.get_content(buf))
+    equals({ "1" }, h:scroll_forward())
   end)
 
   it("should add current buf content to history", function()
-    local h = History:new(buf, { { "1" } })
+    local h = History:new({ { "1" } })
 
-    local items = h:update()
+    local items = h:update(content)
 
     equals({ { content }, { "1" } }, h.items)
     equals({ { content }, { "1" } }, items)
   end)
 
-  it("should update history of current if already saved", function()
-    local h = History:new(buf, { { "1" }, { "2" } })
-    h.saved = true
-
-    local items = h:update()
-
-    equals({ { content }, { "2" } }, h.items)
-    equals({ { content }, { "2" } }, items)
-  end)
-
   it("should update history of the past", function()
-    local h = History:new(buf, { { "1" }, { "2" }, { "3" } })
+    local h = History:new({ { "1" }, { "2" }, { "3" } })
     h.index = 2
 
-    local items = h:update()
+    local items = h:update(content)
 
     equals({ { "1" }, { content }, { "3" } }, h.items)
     equals({ { "1" }, { content }, { "3" } }, items)
   end)
 
   it("should skip the first history item after current content has been saved", function()
-    local h = History:new(buf, { { "1" } })
-    h:update()
+    local h = History:new({ { "1" } })
+    h:update(content)
 
-    h:scroll_back()
-    equals("1", buffer.get_content(buf))
+    local got = h:scroll_back()
+    equals("1", got)
 
-    h:scroll_forward()
-    equals(content, buffer.get_content(buf))
+    got = h:scroll_forward()
+    equals(content, got)
   end)
 end)
